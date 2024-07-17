2a0992ba4db02b124102dcb44789db8caf397fde
SanjidaRifa <133396753+SanjidaRifa@users.noreply.github.com>
2024-07-18 03:49:53 +0600

    Account Setting

--- Inventory_App/Models/AccountSettingMV.cs
+++ Inventory_App/Models/AccountSettingMV.cs
@@ -9,33 +9,39 @@ namespace Inventory_App.Models
     public class AccountSettingMV
     {
         public int AccountSettingID { get; set; }
+        [Required(ErrorMessage = "Required*")]
+      
         public int AccountHeadID { get; set; }
-        [Required(ErrorMessage ="Required*")]
+        
         [Display(Name ="Account Head")]
 
         public string AccountHead { get; set; }
+        [Display(Name = "Account Control")]
         public int AccountControlID { get; set; }
-        [Required(ErrorMessage = "Required*")]
+      
         [Display(Name = "Account Control")]
 
         public string AccountControl { get; set; }
-        public int AccountSubControlID { get; set; }
         [Required(ErrorMessage = "Required*")]
+        public int AccountSubControlID { get; set; }
+
         [Display(Name = "Account Sub Control")]
 
         public string AccountSubControl { get; set; }
-        public int AccountActivityID { get; set; }
         [Required(ErrorMessage = "Required*")]
+        public int AccountActivityID { get; set; }
+    
         [Display(Name = "Account Activity")]
 
         public string AccountActivity { get; set; }
+     
         public int CompanyID { get; set; }
-        [Required(ErrorMessage = "Required*")]
+      
         [Display(Name = "Company")]
 
         public string Company { get; set; }
         public int BranchID { get; set; }
-        [Required(ErrorMessage = "Required*")]
+      
         [Display(Name = "Branch")]
 
         public string Branch { get; set; }
